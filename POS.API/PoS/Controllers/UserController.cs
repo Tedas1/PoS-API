afa8b8cb45e36b5f7d539e575c74265041fbfde7
Tedas <tedodarbo@gmail.com>
2023-12-23 13:46:08 +0200

    User Controller

--- POS.API/PoS/Controllers/UserController.cs
+++ POS.API/PoS/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PoS.Abstractions.Repositories.EntityRepositories;
+using PoS.Entities;
 
 namespace PoS.Controllers
 {
@@ -22,6 +23,45 @@ namespace PoS.Controllers
             var user = await _userRepository.Get(x => x.Id == userId);
 
             return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var allUsers = await _userRepository.GetAll();
+            return Ok(allUsers);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] User user)
+        {
+            await _userRepository.Create(user);
+            await _userRepository.Save();
+            
+            return Created("", user);
+        }
+
+        [HttpPut("{userId}")]
+        public async Task<IActionResult> UpdateUser([FromBody] User user, Guid userId)
+        {
+            if (user.Id != userId) return Conflict();
+
+            _userRepository.Update(user);
+            await _userRepository.Save();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteUser(Guid userId)
+        {
+            if (await _userRepository.Any(x => x.Id == userId))
+            {
+                await _userRepository.Delete(x => x.Id == userId);
+                await _userRepository.Save();
+            }
+
+            return NoContent();
         }
     }
 }
